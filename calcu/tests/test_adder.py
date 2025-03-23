8f7b82bb71fd29c9fdf02036deb15906de18fbf1
charsyam <charsyam@gmail.com>
2025-03-23 18:05:08 +0900

    add more tests

--- calcu/tests/test_adder.py
+++ calcu/tests/test_adder.py
@@ -7,3 +7,12 @@ def test_adder_success():
     adder = Adder(1,2)
     assert adder.execute() == 3
     
+
+def test_adder_success_with_minus_1():
+    adder = Adder(-1,2)
+    assert adder.execute() == 1
+
+
+def test_adder_success_with_minus_2():
+    adder = Adder(-1,-2)
+    assert adder.execute() == -3
