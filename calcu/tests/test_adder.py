ca2802a26c4f6c1fe12653daab0422e1a1a2f291
charsyam <charsyam@gmail.com>
2025-03-23 19:39:48 +0900

    test : 더하기 테스트 추가
     1+2 != 4 에 대한 검증 추가

--- calcu/tests/test_adder.py
+++ calcu/tests/test_adder.py
@@ -6,6 +6,10 @@ from calcu.adder import Adder
 def test_adder_success():
     adder = Adder(1,2)
     assert adder.execute() == 3
+
+def test_adder_should_fail():
+    adder = Adder(1,2)
+    assert adder.execute() != 4
     
 
 def test_adder_success_with_minus_1():
